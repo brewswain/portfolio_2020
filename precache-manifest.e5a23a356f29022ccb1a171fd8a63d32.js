77ca4970d6d56417add1f81f4c4c8ff997ca82b9
Brandon Lee <brandonlee2543@hotmail.com>
2020-11-02 22:19:35 -0400

    Updates

---
+++ precache-manifest.e5a23a356f29022ccb1a171fd8a63d32.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24910b6460d78726b5a90b420797ca76",
+    "url": "/portfolio_2020/index.html"
+  },
+  {
+    "revision": "5bdeb833fd478094fb54",
+    "url": "/portfolio_2020/static/css/main.288a976c.chunk.css"
+  },
+  {
+    "revision": "b2be195c1d0097a3feb5",
+    "url": "/portfolio_2020/static/js/2.07adeca7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolio_2020/static/js/2.07adeca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bdeb833fd478094fb54",
+    "url": "/portfolio_2020/static/js/main.a1ade85c.chunk.js"
+  },
+  {
+    "revision": "c848876db623353e57a8",
+    "url": "/portfolio_2020/static/js/runtime-main.14934412.js"
+  },
+  {
+    "revision": "17bcd4947108d6fa3a2f56af8b3975ec",
+    "url": "/portfolio_2020/static/media/arrow.17bcd494.svg"
+  },
+  {
+    "revision": "7912a0f1cb3ff94373449db60c03e12a",
+    "url": "/portfolio_2020/static/media/blee_box_HomePage_resize.7912a0f1.jpg"
+  },
+  {
+    "revision": "bc71769605a47f3b4b3bcb692d83212b",
+    "url": "/portfolio_2020/static/media/blee_box_fullpage_resize.bc717696.jpg"
+  },
+  {
+    "revision": "18b585c21dcaab7a38c811ddc0308b57",
+    "url": "/portfolio_2020/static/media/blee_box_thumbnail.18b585c2.jpg"
+  },
+  {
+    "revision": "393f03c91991d72634ce6304b36dce89",
+    "url": "/portfolio_2020/static/media/blee_commerce_HomePage_resize.393f03c9.jpg"
+  },
+  {
+    "revision": "f0daf7228b36ed828ac2c39c771e51c8",
+    "url": "/portfolio_2020/static/media/blee_commerce_fullpage_resize.f0daf722.jpg"
+  },
+  {
+    "revision": "a79f1d66b71c4357f4a93c4568b7d088",
+    "url": "/portfolio_2020/static/media/blee_commerce_thumbnail.a79f1d66.jpg"
+  },
+  {
+    "revision": "4f15b0be294e0d12972e43e83b5574de",
+    "url": "/portfolio_2020/static/media/book.4f15b0be.svg"
+  },
+  {
+    "revision": "58bdf68e4030d5442639556b6f11efdf",
+    "url": "/portfolio_2020/static/media/chart.58bdf68e.svg"
+  },
+  {
+    "revision": "7ac1e83e11ee58110e3774014d1fa0d8",
+    "url": "/portfolio_2020/static/media/dashboard_preview.7ac1e83e.jpg"
+  },
+  {
+    "revision": "18b34be8efb16d3bd8dd6584c97f1dfa",
+    "url": "/portfolio_2020/static/media/hat.18b34be8.svg"
+  },
+  {
+    "revision": "fef75ac50b46cdd10cb58c7ab37eb946",
+    "url": "/portfolio_2020/static/media/home.fef75ac5.svg"
+  },
+  {
+    "revision": "f32ed95a2eecf3218683401397f80470",
+    "url": "/portfolio_2020/static/media/logo.f32ed95a.svg"
+  },
+  {
+    "revision": "9dabb7d1e1b7daa61da558b7b124bee7",
+    "url": "/portfolio_2020/static/media/menu.9dabb7d1.svg"
+  },
+  {
+    "revision": "aae192b4c4af068b8031874e57220bff",
+    "url": "/portfolio_2020/static/media/phone.aae192b4.svg"
+  },
+  {
+    "revision": "09a11131f03c70048fd12ee0468d0391",
+    "url": "/portfolio_2020/static/media/queeery_fullpage_resize.09a11131.jpg"
+  },
+  {
+    "revision": "81cb1098a364a135ca37ab8270a82820",
+    "url": "/portfolio_2020/static/media/queeery_mission_resize.81cb1098.jpg"
+  },
+  {
+    "revision": "87ff077bda330b70049216783e37aac3",
+    "url": "/portfolio_2020/static/media/queeery_thumbnail.87ff077b.jpg"
+  },
+  {
+    "revision": "8afbfc4e025296fee1e33a24571c0022",
+    "url": "/portfolio_2020/static/media/tcl_hero_1_resize.8afbfc4e.jpg"
+  },
+  {
+    "revision": "e0b4621d29dcb8d67c00d84429dd2a02",
+    "url": "/portfolio_2020/static/media/tcl_hero_2_resize.e0b4621d.jpg"
+  },
+  {
+    "revision": "95a62d16f02d4fcdb11a13f912ada9cd",
+    "url": "/portfolio_2020/static/media/tcl_homepage_resize.95a62d16.jpg"
+  },
+  {
+    "revision": "a9603c775d03f8840a6694aeaeba267d",
+    "url": "/portfolio_2020/static/media/tcl_thumbnail.a9603c77.jpg"
+  },
+  {
+    "revision": "7a28779e562caec03124a1fc8161de47",
+    "url": "/portfolio_2020/static/media/terminal.7a28779e.svg"
+  }
+]);
